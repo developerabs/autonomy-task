808ef5a51d795cbedd5e028a5be48b8bf744af15
developerabs <bokkor3256@gmail.com>
2023-06-16 21:47:08 +0600

    category crud added

---
+++ app/Models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $dates = ['deleted_at'];
+   
+    protected $fillable = ['parent_id', 'level', 'cat_name', 'slug', 'icon', 'banner', 'featured', 'title', 'description', 'status'] ; 
+
+    public function subChildCategoriy()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+    public function childCategoriy()
+    {
+        return $this->hasMany(Category::class, 'parent_id')->with('subChildCategoriy');
+    }
+    public function subCategoriy()
+    {
+        return $this->hasMany(Category::class, 'parent_id')->with('childCategoriy');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+  
+}
